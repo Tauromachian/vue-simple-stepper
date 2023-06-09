ab49133d48ae4f88c13bed33d9a77b73b80a49f6
Jose Garcia <jogarcia@tuta.io>
2023-06-09 16:34:20 -0400

    Make base-divider component

---
+++ src/components/BaseDivider.vue
@@ -0,0 +1,28 @@
+<template>
+  <hr class="divider" :style="{ 'background-color': color }" />
+</template>
+
+<script setup>
+defineProps({
+  vertical: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style scoped>
+.divider {
+  height: 2px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: var(--secondary-color);
+  border: 0;
+  border-radius: 10px;
+  width: 100%;
+}
+</style>
