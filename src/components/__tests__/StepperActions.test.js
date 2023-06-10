509e35194b12d7ec0068bde90133281742129ebf
Jose Garcia <jogarcia@tuta.io>
2023-06-10 16:08:33 -0400

    Add test for stepper actions component

---
+++ src/components/__tests__/StepperActions.test.js
@@ -0,0 +1,38 @@
+import { mount } from "@vue/test-utils";
+import StepperActions from "../StepperActions.vue"; // Update with your component path
+
+describe("StepperActions", () => {
+  it("renders correctly", () => {
+    const wrapper = mount(StepperActions);
+
+    expect(wrapper.find(".buttons-container").exists()).toBe(true);
+    expect(wrapper.find(".vue-easy-stepper-button").exists()).toBe(true);
+  });
+
+  it("emits click:next event when Next button is clicked", async () => {
+    const wrapper = mount(StepperActions, {
+      props: { isLastStep: false },
+    });
+    await wrapper.find(".vue-easy-stepper-button").trigger("click");
+
+    expect(wrapper.emitted()).toHaveProperty("click:next");
+  });
+
+  it("emits click:previous event when Previous button is clicked", async () => {
+    const wrapper = mount(StepperActions, {
+      props: { isFirstStep: false },
+    });
+    await wrapper.find(".vue-easy-stepper-button--outlined").trigger("click");
+
+    expect(wrapper.emitted()).toHaveProperty("click:previous");
+  });
+
+  it("emits click:submit event when Submit button is clicked", async () => {
+    const wrapper = mount(StepperActions, {
+      props: { isLastStep: true },
+    });
+    await wrapper.find(".vue-easy-stepper-button").trigger("click");
+
+    expect(wrapper.emitted()).toHaveProperty("click:submit");
+  });
+});
