32c30ef3bc3ffbd02f98df276f58dcb0ba7b3cf4
Jose Garcia <jogarcia@tuta.io>
2023-06-10 16:07:43 -0400

    Remove unnecessary prop

--- src/components/StepperActions.vue
+++ src/components/StepperActions.vue
@@ -3,8 +3,6 @@
     <button
       v-if="!isLastStep"
       color="primary"
-      :disabled="disabled"
-      :loading="disabled"
       @click="clickNext"
       class="vue-easy-stepper-button"
     >
@@ -14,7 +12,6 @@
     <button
       v-else
       @click="clickSubmit"
-      :loading="disabled"
       color="primary"
       class="vue-easy-stepper-button"
     >
@@ -23,13 +20,11 @@
 
     <button
       v-if="!isFirstStep"
-      :disabled="disabled"
       color="primary"
       variant="text"
       @click="clickPrevious"
       outlined
       class="mr-2 vue-easy-stepper-button vue-easy-stepper-button--outlined"
-      :loading="disabled"
     >
       Previous
     </button>
@@ -43,10 +38,6 @@ defineProps({
     default: false,
   },
   isFirstStep: {
-    type: Boolean,
-    default: false,
-  },
-  disabled: {
     type: Boolean,
     default: false,
   },
