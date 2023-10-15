5ee0893d84afced1650ed1e5a8cec83289a467f8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-10-15 11:26:39 -0400

    Add new stepsState state

--- component/src/components/AppStepper.vue
+++ component/src/components/AppStepper.vue
@@ -26,6 +26,7 @@ const emit = defineEmits(["click:next", "click:previous", "click:submit"]);
 
 const state = reactive({
   stepHasBeenVisited: [],
+  stepsState: {}
 });
 
 const slot = useSlots();
