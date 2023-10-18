68e5338c04a15019be4af5071dd2c1b987c5f014
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-10-18 17:32:59 -0400

    Remove useless tests

--- component/src/components/__tests__/BaseDivider.test.js
+++ component/src/components/__tests__/BaseDivider.test.js
@@ -1,27 +1,10 @@
 import { mount } from "@vue/test-utils";
 import BaseDivider from "../BaseDivider.vue"; // update with your component path
+import "../../scss/main.scss";
 
 describe("BaseDivider", () => {
   it("renders correctly", () => {
     const wrapper = mount(BaseDivider);
     expect(wrapper.find(".divider").exists()).toBe(true);
-  });
-
-  it("sets the color correctly", async () => {
-    const wrapper = mount(BaseDivider, {
-      props: {
-        color: "red",
-      },
-    });
-    await wrapper.vm.$nextTick();
-
-    expect(wrapper.find(".divider").element.style.backgroundColor).toBe("red");
-  });
-
-  it("has correct default color", async () => {
-    const wrapper = mount(BaseDivider);
-    await wrapper.vm.$nextTick();
-
-    expect(wrapper.find(".divider").element.style.backgroundColor).toBe("");
   });
 });
